d6f4ad096e563b99dbba2ae67f02ce4f706a2df5
HannachiHassen <hannachihassen07@gmail.com>
2022-08-26 18:15:25 -0400

    AssertJ - Fluent Assertion Java Library in HomepageTest

--- src/main/java/com/tmb/constans/FrameworkConstants.java
+++ src/main/java/com/tmb/constans/FrameworkConstants.java
@@ -2,13 +2,21 @@ package com.tmb.constans;
 
 public final class FrameworkConstants {
 	
-	private static final String CHROMEDRIVERPATH= System.getProperty("user.dir") +"/src/test/resources/executables/chromedriver.exe";
-		
-	private FrameworkConstants() {
+    private FrameworkConstants() {
 		
 	}
 	
+    private static final String RESOURCESPATH= System.getProperty("user.dir") +"/src/test/resources/";
+	private static final String CHROMEDRIVERPATH= RESOURCESPATH + "/executables/chromedriver.exe";
+	private static final String CONFIGFILEPATH= RESOURCESPATH + "/config/config.properties";
+	
+	
+	
 	public static String getChromeDriverpath() {
 		return CHROMEDRIVERPATH;
+	}
+
+	public static String getConfigfilepath() {
+		return CONFIGFILEPATH;
 	}
 }
