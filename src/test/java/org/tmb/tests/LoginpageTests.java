0b95a55d347a60b9f5fdf394ddfb62cb01399c5d
HannachiHassen <hannachihassen07@gmail.com>
2022-08-26 12:11:40 -0400

    Create BeforeMethod, AfterMethod

--- src/test/java/org/tmb/tests/LoginpageTests.java
+++ src/test/java/org/tmb/tests/LoginpageTests.java
@@ -4,37 +4,34 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.tmb.constans.FrameworkConstants;
 
 public class LoginpageTests {
 	
-	//1. Never Hardcode
-	//2. keep right thing at right place
-		
-	// class or interface name ---> AmuthanSakthivel
-	// final constant values --> AMUTHANSAKTHIVEL or AMUTHAN_SAKTHIVEL
-	// method ---> amuthanSakthivel
-	// package --> amuthansakathivel
+	WebDriver driver;
 	
-	@Test
-	public void test1() {
+	@BeforeMethod
+	public void setUp() {
 		System.setProperty("webdriver.chrome.driver", FrameworkConstants.getChromeDriverpath());
-		WebDriver driver= new ChromeDriver();
-		
+		WebDriver driver= new ChromeDriver();		
 		driver.get("https://google.com");
-		driver.findElement(By.name("q")).sendKeys("Automation", Keys.ENTER) ;
-		driver.close();		
 	}
 	
 	@Test
+	public void test1() {
+		driver.findElement(By.name("q")).sendKeys("Automation", Keys.ENTER) ;	}
+	
+	@Test
 	public void test2() {
-		System.setProperty("webdriver.chrome.driver", FrameworkConstants.getChromeDriverpath());
-		WebDriver driver= new ChromeDriver();
-		
-		driver.get("https://google.com");
 		driver.findElement(By.name("q")).sendKeys("testing mini bytes", Keys.ENTER);
+	}
+	
+	@AfterMethod
+	public void tearDown() {
 		driver.close();	
 	}
 }
