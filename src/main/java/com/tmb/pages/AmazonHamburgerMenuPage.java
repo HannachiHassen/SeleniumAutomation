49aa8872128ed4fcf21a894268d03f3ea41a5848
HannachiHassen <hannachihassen07@gmail.com>
2022-09-05 21:57:10 -0400

    Update 'AmazonHamburgareMenuPage' class by remove all the mothodes and replace them with just one clickOnSubMenuItem instead

--- src/main/java/com/tmb/pages/AmazonHamburgerMenuPage.java
+++ src/main/java/com/tmb/pages/AmazonHamburgerMenuPage.java
@@ -1,8 +1,6 @@
 package com.tmb.pages;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import com.tmb.driver.DriverManager;
@@ -10,18 +8,6 @@ import com.tmb.enums.WaitStrategy;
 import com.tmb.utils.DynamicXpathUtils;
 
 public final class AmazonHamburgerMenuPage extends BasePage {
-
-	@FindBy(xpath = "//div[text()='Mobiles, Computers']/parent::a")
-	private WebElement linkMobileAndComputers;
-	
-	@FindBy(xpath = "//a[text()='laptops']")
-	private WebElement linkLaptops;
-	
-	@FindBy(xpath = "//a[text()='Drivers & Storage']")
-	private WebElement linkDriversAndStorage;
-	
-	@FindBy(xpath = "//a[text()='Printer & Ink']")
-	private WebElement linkPrinterAndInk;
 	
 	private String linkSubMenu= "//a[text()='%replaceable%']";
 	
@@ -32,25 +18,5 @@ public final class AmazonHamburgerMenuPage extends BasePage {
 	public void clickOnSubMenuItem(String menutext) {
 		String newxpath=DynamicXpathUtils.getXpath(linkSubMenu, menutext);
 		click(By.xpath(newxpath), WaitStrategy.CLICKABLE, menutext);
-	}
-
-	public AmazonHamburgerMenuPage clickMobileAndComputers() {
-		linkMobileAndComputers.click();
-		return this;
-	}
-
-	public AmazonLaptopPage clickLinkLaptops() {
-		linkLaptops.click();
-		return new AmazonLaptopPage();
-	}
-
-	public AmazonHamburgerMenuPage clickDriversAndStorage() {
-		linkDriversAndStorage.click();
-		return this;
-	}
-
-	public AmazonHamburgerMenuPage clickPrinterAndInk() {
-		linkPrinterAndInk.click();
-		return this;
 	}
 }
