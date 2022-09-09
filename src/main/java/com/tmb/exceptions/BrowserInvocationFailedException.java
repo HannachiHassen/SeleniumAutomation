a26be1fa7eedcc0c78f15f680fa589d27b083bb0
HannachiHassen <hannachihassen07@gmail.com>
2022-09-08 21:25:16 -0400

    Updae 'BrowserInvocationFailedException' class

--- src/main/java/com/tmb/exceptions/BrowserInvocationFailedException.java
+++ src/main/java/com/tmb/exceptions/BrowserInvocationFailedException.java
@@ -6,5 +6,9 @@ public class BrowserInvocationFailedException extends FrameworkException{
 	public BrowserInvocationFailedException(String message) {
 		super(message);
 	}
+	
+	public BrowserInvocationFailedException(String message,Throwable cause) {
+		super(message,cause);
+	}
 
 }
