c28af8ba008b9c1f7e9c60922fd68bb91a1b1bc7
HannachiHassen <hannachihassen07@gmail.com>
2022-09-08 21:19:16 -0400

    Handle exception of Browser 'BrowserInvocationFailedException' class

---
+++ src/main/java/com/tmb/exceptions/BrowserInvocationFailedException.java
@@ -0,0 +1,10 @@
+package com.tmb.exceptions;
+
+@SuppressWarnings("serial")
+public class BrowserInvocationFailedException extends FrameworkException{
+
+	public BrowserInvocationFailedException(String message) {
+		super(message);
+	}
+
+}
