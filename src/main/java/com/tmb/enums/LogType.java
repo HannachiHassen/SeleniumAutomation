b04cc8e621db33d23fea6b4b37c75a1d943fc89a
HannachiHassen <hannachihassen07@gmail.com>
2022-09-24 13:48:28 -0400

    Add new class FrameworkLogger and new enum class LogType

---
+++ src/main/java/com/tmb/enums/LogType.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.tmb.enums;
+
+/**
+ * Sep 24, 2022
+ * @author HASSEN
+ * @version 1.0
+ * @since 1.0
+ */
+public enum LogType {
+	PASS, FAIL, SKIP, INFO, CONSOLE, EXTENTANDCONSOLE
+
+}
